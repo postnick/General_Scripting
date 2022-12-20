d3338a70ab53aa0e6681ae0ff6abe249d726b9a7
Nick <postnick@users.noreply.github.com>
2022-12-19 21:42:11 -0600

    Update configure-dnf.sh
    Posted this script on reddit - got good feedback about how to check to make sure the text is there or not.

--- configure-dnf.sh
+++ configure-dnf.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+#Script now checks to see if the text is in the file before adding it. Thanks reddit user /u/biggle-tiddie
+
 cd ~
 
-# allways checks for the fastest mirrors 
-sudo echo "fastestmirror=True" >> /etc/dnf/dnf.conf
+# always checks for the fastest mirrors 
+if ! grep ^fastestmirror /etc/dnf/dnf.conf; then
+      echo "fastestmirror=True" | sudo tee -a /etc/dnf/dnf.conf
+fi
 
-# Enables dnf to download multiple packages at the same time, in this case 5 at a time.
+# Enables DNF to download multiple packages at the same time, in this case 5 at a time.
 # Depending on your internet connection you might want to set it to a higher number. 
-sudo echo "max_parallel_downloads=10" >> /etc/dnf/dnf.conf
+if ! grep ^max_parallel_downloads /etc/dnf/dnf.conf; then
+      echo "max_parallel_downloads=10"  | sudo tee -a /etc/dnf/dnf.conf
+fi
 
 # sets [Y/n] as default
-sudo echo "defaultyes=True" >> /etc/dnf/dnf.conf
+if ! grep ^defaultyes /etc/dnf/dnf.conf; then
+      echo "defaultyes=True" | sudo tee -a /etc/dnf/dnf.conf
+fi
 
-#Got this from Tech Hut
-sudo echo "keepcache=True" >> /etc/dnf/dnf.conf
+# always checks for the fastest mirrors 
+if ! grep ^keepcache /etc/dnf/dnf.conf; then
+      echo "keepcache=True" | sudo tee -a /etc/dnf/dnf.conf
+fi
