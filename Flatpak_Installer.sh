5eaeb5cc0557df90e9a27f24a243812fef36fbf7
Nick <8466253+postnick@users.noreply.github.com>
2023-10-09 10:26:13 -0500

    Update Flatpak_Installer.sh
    Removed a few that I just don't use.

--- Flatpak_Installer.sh
+++ Flatpak_Installer.sh
@@ -14,24 +14,15 @@
     flatpak install flathub com.mattjakeman.ExtensionManager -y
 #Obfuscate - Hide things in photos
     flatpak install flathub com.belmoussaoui.Obfuscate -y
-#Keeps the top bar dark to match system colors
-    flatpak install flathub com.github.bluesabre.darkbar -y
 #Blanket is background noises and sounds
     flatpak install flathub com.rafaelmardojai.Blanket -y
 #Spotify
     flatpak install flathub com.spotify.Client -y
-#Steam link to play steam games off of other computers
-    flatpak install flathub com.valvesoftware.SteamLink -y
-#Markdown Editor
-    flatpak install flathub io.posidon.Paper -y
 #Markdown Editor - Better
     flatpak install flathub md.obsidian.Obsidian -y
 #Podcasts
     flatpak install flathub org.gnome.Podcasts -y
-#Todoist - a todo list I love
-    flatpak install flathub com.todoist.Todoist -y
 #flatseal - a flatpak manager
     flatpak install com.github.tchx84.Flatseal -y
 #OBS - Screen Recording software
     flatpak install com.obsproject.Studio -y
-
